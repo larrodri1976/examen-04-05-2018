f1f860ec3aa8f4a24b557a640a3254ab26348933
Rafael <rcarlosama@misena.edu.co>
2018-05-04 20:50:55 -0500

    subiendo examen

---
+++ Examen/examen 040518.js
@@ -0,0 +1,9 @@
+function result() {
+            var n1 = document.getElementById('txtN1').value;
+            var n2 = document.getElementById('txtN2').value;
+            var n3 = document.getElementById('txtN3').value;
+            var result = parseInt(n1) + parseInt(n2);
+            alert("El primer resultado es:: "+result)
+        }
+
+       
